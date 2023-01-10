e17350c50eaa7fc647cd5db61a7255b18b11c462
João Gouveia <joao.gouveia2@pt.bosch.com>
2023-01-10 13:00:02 +0000

    change ticket index last update, to show the last daily update or the ticket last update, depending on which one is more recent

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,6 +4,14 @@ class TicketsController < ApplicationController
   # GET /tasks or /tasks.json
   def index
     @tickets = Ticket.all
+
+    @last_update = Hash.new
+
+    @tickets.each do|ticket|
+      # if there is a ticket daily which is most recent comparing to ticket update, the last update is this daily date
+      last_daily = Daily.where(ticket_id: ticket.id).order(updated_at: :desc).limit(1).first
+      @last_update[ticket.id] = [last_daily.updated_at, ticket.updated_at].max
+    end
   end
 
   # GET /tasks/1 or /tasks/1.json
