434863c524a12cf9d41a49851699f846c517897a
Joao Gouveia <goj2lis@lis-c-000wk.home>
2023-03-22 13:00:17 +0000

    added helper class for today and yesterday string on dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def date_ty_simplifier date
+        now = Time.now.to_date
+        date = date.to_date
+        if date == now
+            "hoje"
+        elsif date == (now - 1)
+            "ontem"
+        else
+            date.strftime("%d/%m/%Y")
+        end
+    end
 end
