872a49aa2073340152603005ba7fc22aed9b9605
Joao Gouveia <goj2lis@lis-c-000wk.home>
2023-03-30 13:10:32 +0100

    created ticket index page, preparing for the links section

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,6 +8,7 @@ class TicketsController < ApplicationController
 
   # GET /tasks/1 or /tasks/1.json
   def show
+    @last_dailies = Daily.order(:status, "updated_at DESC").limit(3)
   end
 
   # GET /tasks/new
