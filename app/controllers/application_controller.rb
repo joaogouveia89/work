8c5c2b4ccc268556c6671e76946b775ca85e6dbe
Joao Gouveia <goj2lis@lis-c-000wk.lan>
2023-04-28 11:24:16 +0100

    Added last updates modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :last_updates_info , only: %i[ index ]
+
     def today
         Time.now
+    end
+
+    def last_updates_info
+        last_updates_git_command = `git log -1 --date=format:'%d/%m/%Y' --format="%cd - %B"`
+        last_updates_git_command = last_updates_git_command.split('-')
+        @last_updates = {:date => last_updates_git_command[0].strip, :commit_message => last_updates_git_command[1].strip.humanize }
     end
 end
